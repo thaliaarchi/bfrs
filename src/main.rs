8fdc426c3688f74af50a1fe83fd4f973058354e6
Thalia Archibald <thalia@archibald.dev>
2024-09-12 03:28:01 -0700

    Create simple main

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::{env, fs, process};
+
+use bfrs::{graph::Graph, ir::Ir, Ast};
+
+fn main() {
+    let args = env::args_os();
+    if args.len() != 2 {
+        eprintln!("Usage: bfrs <PROGRAM>");
+        process::exit(2);
+    }
+    let path = args.skip(1).next().unwrap();
+    let src = match fs::read(&path) {
+        Ok(src) => src,
+        Err(err) => {
+            eprintln!("read {}: {err}", path.to_string_lossy());
+            process::exit(2);
+        }
+    };
+    let Some(ast) = Ast::parse(&src) else {
+        eprintln!("parse error");
+        process::exit(1);
+    };
+    let mut g = Graph::new();
+    let mut ir = Ir::lower(&ast, &mut g);
+    Ir::optimize_root(&mut ir, &mut g);
+    print!("{}", Ir::pretty_root(&ir, &g));
+}
